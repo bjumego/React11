4bd15899191d620f76ad40d7e51321f094391b8f
janikanika <janikanika@mail.ru>
2021-09-30 08:54:52 +0300

    ne rabotaet

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,2 @@
-import App from '/src/app/app'
+import App from './app'
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import  ReactDOM  from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import App from './components/app/';
+import App from '.app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
